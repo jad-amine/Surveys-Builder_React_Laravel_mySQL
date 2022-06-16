0072cf0796e9074041daed85f83d7e0985d9a9f6
jad amine <jade1992amine@gmail.com>
2022-06-16 23:16:10 +0300

    Linked Back and Front End

--- react.js/src/App.js
+++ react.js/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+import { useState } from "react";
 import './App.css';
 
 function App() {
+  const [data, setData] = useState('');
+  const fetchServerData = async (url) => {
+    const res = await fetch(url);
+    const data = await res.json();
+    setData(data);
+    return data;
+  } 
+  fetchServerData('http://localhost:8000/api/')
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {data ? JSON.stringify(data) :'hi'}
+    </>
   );
 }
 
