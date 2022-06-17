3e5d13e628625e05475ca95043e4adc17dcd6e73
jad amine <jade1992amine@gmail.com>
2022-06-17 14:41:16 +0300

    Added Answer API to store answers

--- laravel-backend/app/Http/Controllers/UserController.php
+++ laravel-backend/app/Http/Controllers/UserController.php
@@ -3,13 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Answer;
 
 class UserController extends Controller{
 
-    public function addAnswer(){
+    public function addAnswer(Request $resquest){
+        $answer = new Answer;
+        $answer->user_id = $resquest->user_id;
+        $answer->question_id = $resquest->question_id;
+        $answer->answer = $resquest->answer;
+        $answer->survey_name = $resquest->survey_name;
+        $answer->save();
+
         return response()->json([
             "status" => "success",
-            "message" => "Answer added"
+            "content" => $answer
         ], 200);
     }
 }
