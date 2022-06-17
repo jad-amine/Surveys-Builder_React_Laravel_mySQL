268b52b7d4a690afb5ae9ed9e7c99e8a177154dd
jad amine <jade1992amine@gmail.com>
2022-06-17 14:19:44 +0300

    Added survey API to store questions

--- laravel-backend/app/Http/Controllers/AdminController.php
+++ laravel-backend/app/Http/Controllers/AdminController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use App\Models\Question;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller{
@@ -18,6 +19,22 @@ class AdminController extends Controller{
         return response()->json([
             "status" => "success",
             "users" => $users
+        ], 200);
+    }
+
+    // Add a survey
+
+    // Fix this issue with json and surveys filling 
+    public function store(Request $request){
+        $question = new Question;
+        $question->type = $request->type;
+        $question->content = $request->content;
+        $question->possible_answers = $request->possible_answers;
+        $question->survey_name = $request->survey_name;
+        $question->save();
+        return response()->json([
+            "status" => "success",
+            "message" => "question saved"
         ], 200);
     }
 }
