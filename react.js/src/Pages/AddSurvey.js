f6e4e1b92725da7e3ca98359bf093b9e84a60c9c
jad amine <jade1992amine@gmail.com>
2022-06-21 22:25:13 +0300

    Finished Add Survey page and functionality

--- react.js/src/Pages/AddSurvey.js
+++ react.js/src/Pages/AddSurvey.js
@@ -11,15 +11,79 @@ const AddSurvey = () => {
     },
   ]);
 
+  //Add a new question field
+  const addFields = (e) => {
+    e.preventDefault();
+    let newField = {
+      label: "",
+      type: "textBox",
+      choices: [],
+      currentChoice: "",
+    };
+    setInputFields([...inputFields, newField]);
+  };
+
+  // Dynamically change and save Admin inputs
   const handleFormChange = (index, event) => {
     const data = [...inputFields];
     data[index][event.target.name] = event.target.value;
     setInputFields(data);
   };
 
-  const types = ["dropDown", "checkBox", "textBox", "radio"];
+  // Add Choices to dropdown and radio questions
+  const handelChoiceAdd = (index, newChoice) => {
+    const data = [...inputFields];
+    data[index].choices.push(newChoice);
+    setInputFields(data);
+  };
+
+  // Send data to the backend
+  const handelSubmit = async () => {
+    const res = await fetch("http://localhost:8000/api/addSurvey", {
+      method: "POST",
+      headers: {
+        Authorization: Bearer + localStorage.getItem("token"),
+      },
+      body: JSON.stringify({ title: surveyTitle }),
+    });
+    const data = await res.json();
+    const ID = data.id;
+    inputFields.forEach(async (inputField) => {
+      const res2 = await fetch("http://localhost:8000/api/addQuestion", {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+          Authorization: Bearer + localStorage.getItem("token"),
+        },
+        body: JSON.stringify({
+          label: inputField.label,
+          type: inputField.type,
+          survey_id: ID,
+        }),
+      });
+      const data2 = await res2.json();
+      const question_id = data2.id;
+      inputField.choices.forEach(async (choice) => {
+        const res3 = await fetch("http://localhost:8000/api/addQuestion", {
+          method: "POST",
+          headers: {
+            "Content-type": "application/json",
+            Authorization: Bearer + localStorage.getItem("token"),
+          },
+          body: JSON.stringify({
+            question_id: question_id,
+            text: choice,
+          }),
+        });
+        const data3 = await res3.json();
+      });
+    });
+  };
+
+  const types = ["checkBox", "dropDown", "textBox", "radio"];
+
   return (
-    <form>
+    <form onSubmit={handelSubmit}>
       <h2>Survey Title</h2>
       <input
         type="text"
@@ -54,12 +118,31 @@ const AddSurvey = () => {
           {/* Question Choices */}
           {(input.type === "dropDown" || input.type === "radio") && (
             <div>
-            <p>Choices:</p>
-              <input type="text" value={input.currentChoice}/>
+              <p>Choices:</p>
+              <input
+                type={"text"}
+                name={"currentChoice"}
+                onChange={(e) => handleFormChange(index, e)}
+              />
+              <button
+                type="button"
+                onClick={(e) => {
+                  handelChoiceAdd(index, input.currentChoice);
+                }}
+              >
+                Add Choice
+              </button>
+              {input.choices.map((choice) => {
+                return <div>{choice}</div>;
+              })}
             </div>
           )}
         </div>
       ))}
+      <button type="button" onClick={addFields}>
+        Add more
+      </button>
+      <button type="submit"> Add Survey</button>
     </form>
   );
 };
